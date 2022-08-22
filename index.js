1f4872e0f501086576b69888b1f7c83e93d591c2
Aanand-shukla <93906271+Aanand-shukla@users.noreply.github.com>
2022-08-22 18:03:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// difference let vs block
+
+// var firstName ="Aanand";
+// console.log(firstName);
+// var firstName ="shukla";
+// console.log(firstName);
+//   let variable can't redeclare
+let firstName ="Aanand";
+console.log(firstName);
+ firstName ="shukla";
+console.log(firstName);
